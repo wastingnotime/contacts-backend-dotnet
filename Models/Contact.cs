6b6d03bfefce598e5056dc9663f35a500994f10c
henrique <henrique@codingzen.org>
2023-04-06 20:05:03 -0300

    small improvements

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -2,7 +2,7 @@ namespace ContactsBackendDotnet.Models
 {
     public class Contact
     {
-        public Guid Id { get; }= Guid.NewGuid();
+        public Guid Id { get; set; }= Guid.NewGuid();
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
