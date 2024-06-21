590820ae3ed3e581f7fa65e7ad63c954d2c7887f
hriccio <hriccio@wastingnotime.org>
2024-06-21 08:15:24 -0300

    change style: namespace

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -3,71 +3,70 @@ using ContactsBackendDotnet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ContactsBackendDotnet.Controllers
+namespace ContactsBackendDotnet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ContactsController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ContactsController : ControllerBase
+    private readonly ILogger<ContactsController> _logger;
+    private readonly ContactContext _context;
+
+    public ContactsController(ILogger<ContactsController> logger, ContactContext context)
     {
-        private readonly ILogger<ContactsController> _logger;
-        private readonly ContactContext _context;
+        _logger = logger;
+        _context = context;
+    }
 
-        public ContactsController(ILogger<ContactsController> logger, ContactContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Contact>>> Get() => 
+        await _context.Contacts.ToListAsync();
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Contact>>> Get() => 
-            await _context.Contacts.ToListAsync();
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<Contact>> Get(Guid id)
+    {
+        var item = await _context.Contacts.FindAsync(id);
+        return item == null ? NotFound() : item;
+    }
 
-        [HttpGet("{id:guid}")]
-        public async Task<ActionResult<Contact>> Get(Guid id)
-        {
-            var item = await _context.Contacts.FindAsync(id);
-            return item == null ? NotFound() : item;
-        }
+    [HttpPost]
+    public async Task<IActionResult> Post(Contact value)
+    {
+        _context.Contacts.Add(value);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(Get), new { id = value.Id }, value);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Post(Contact value)
-        {
-            _context.Contacts.Add(value);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(Get), new { id = value.Id }, value);
-        }
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Contact value, Guid id)
+    {
+        if (!id.Equals(value.Id))
+            return BadRequest();
 
-        [HttpPut("{id:guid}")]
-        public async Task<IActionResult> Update(Contact value, Guid id)
-        {
-            if (!id.Equals(value.Id))
-                return BadRequest();
+        var item = await _context.Contacts.FindAsync(id);
+        if (item is null)
+            return NotFound();
 
-            var item = await _context.Contacts.FindAsync(id);
-            if (item is null)
-                return NotFound();
+        item.FirstName = value.FirstName;
+        item.LastName = value.LastName;
+        item.PhoneNumber = value.PhoneNumber;
 
-            item.FirstName = value.FirstName;
-            item.LastName = value.LastName;
-            item.PhoneNumber = value.PhoneNumber;
+        _context.Contacts.Update(item);
+        await _context.SaveChangesAsync();
 
-            _context.Contacts.Update(item);
-            await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-            return NoContent();
-        }
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var item = await _context.Contacts.FindAsync(id);
+        if (item is null)
+            return NotFound();
 
-        [HttpDelete("{id:guid}")]
-        public async Task<IActionResult> Delete(Guid id)
-        {
-            var item = await _context.Contacts.FindAsync(id);
-            if (item is null)
-                return NotFound();
+        _context.Contacts.Remove(item);
+        await _context.SaveChangesAsync();
 
-            _context.Contacts.Remove(item);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,12 +1,11 @@
-namespace ContactsBackendDotnet.Models
+namespace ContactsBackendDotnet.Models;
+
+public class Contact
 {
-    public class Contact
-    {
-        public Guid Id { get; set; }= Guid.NewGuid();
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public Contact Clone() =>
-            (Contact)MemberwiseClone();
-    }
+    public Guid Id { get; set; }= Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public Contact Clone() =>
+        (Contact)MemberwiseClone();
 }
